caf0c01de64eb8e7a2522b40717b1d3debeb435c
ranckprogram <1474574022@qq.com>
2018-08-30 23:18:57 +0800

    html-wabpack-plugin 练习

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
+	// context: '', // 上下文，运行配置的上下文
 	mode: 'production',
 	entry: './src/index.js',
 	output: {
 		path: __dirname + '/dist',
-		filename: 'mari.js'
-	}
+		filename: 'js/mari.js'
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			// filename: 'index-[hash].html',
+			filename: 'index.html',
+			template: 'index.html',
+			title: 'ranck home page'
+		})
+	]
 }
